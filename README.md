7e805c164335f737ebb63d9abbb5fd34d3be8be3
tom-zhao <tom_zhao_vip@163.com>
2015-08-14 16:52:56 +0800

    process the cmd arguments

--- README.md
+++ README.md
@@ -2,4 +2,7 @@ ab.py is python port of ab
 ===================
 
 ab (aka apache benchmark) is widely-used tool for benching http server.    
-now I rebuild the software in python, while it 's only works in Linux,because of the epoll system call.
+
+now I rebuild the software in python, while it 's only works in Linux,
+
+because of the epoll system call.
--- src/ab.py
+++ src/ab.py
@@ -1,37 +1,106 @@
 #!/usr/bin/python
+import getopt
+import sys
 
-import argparse
-
+program_info = 'ab.py is python port of ab'
+version = '0.0.1'
+copyright = 'Copyright 2015 tom zhao'
 # control the connectins
 class connections(object):
-    pass
-
-# the cmd input argument.
-class params(object):
     def __init__(self):
-        self.recverrok=True
+        pass
+
+
+# the cmd input arguments.
+class arguments(object):
+    def __init__(self):
+        self.requests_count = 1
+        self.concurrency = 1
+        self.timelimit = 0
+        self.timeout = 30
+        self.windowsize = 0
+        self.content_type = "text/plain"
+        self.keep_alive = False
+        self.print_version = False
+        self.print_usage = False
+
+class  connection_times(object):
+    __slots__ = ('start_time', 'wait_time', 'con_time', 'time')
+    #start_time   time of starting the connection 
+    #wait_time     time of between request and response.
+    #connect_time   time to connect
+    #time           time for connection
+
+# the result of test
+class result(object):
+    def __init__(self):
+        pass
+
+    def print_result(params):
+        pass
     
+ 
 def test():
     pass
 
 def arguments_parse():
-    
-    print(args)
-    return args
+    params = arguments()
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "n:c:t:s:T:Vh", [])
+    except getopt.GetoptError as err:
+        print("ab.py: wrong number of arguments")
+        usage(sys.argv[0])
+        sys.exit(2)
 
-def validate_arguments(args):
-    """
-    valide the arguments
-    return args is 
-    """
-    return (True, None)
+    for arg, val in opts:
+        if arg == "-n" : params.requests_count = int(val)
+        if arg == "-c" : params.concurrency = int(val)
+        if arg == "-t" : params.timelimit = int(val)
+        if arg == "-s" : params.timeout = int(val)
+        if arg == "-T" : params.content_type = val
+        if arg == "-h" : params.print_usage = True
+        if arg == "-V" : params.print_version = True
+        if arg == "-k" : params.keep_alive = True
+
+    
+    if len(args) == 1:
+        params.url = args[0]
+    else:
+        print("ab: wrong number of arguments")
+        sys.exit(2)
+
+    if params.print_version:
+        print_version()
+        sys.exit(2)
+    if params.print_usage:
+        usage(sys.argv[0])
+        sys.exit(2)       
+    
+    return params
+
+def print_version():
+    print("%s, Version %s \n %s" %(program_info, version, copyright))
+
+def usage(prog_name):
+    print("Usage: %s [options] [http[s]://]hostname[:port]/path\n" %prog_name)
+    print("Options are:")
+    print("""
+    -n requests     Number of requests to perform
+    -c concurrency  Number of multiple requests to make at a time
+    -t timelimit    Seconds to max. to spend on benchmarking
+                    This implies -n 50000
+    -s timeout      Seconds to max. wait for each response
+                    Default is 30 seconds
+    -b windowsize   Size of TCP send/receive buffer, in bytes
+    -T content-type Content-type header to use for POST/PUT data, eg.
+                    'application/x-www-form-urlencoded'
+                    Default is 'text/plain'
+    -k              Use HTTP KeepAlive feature
+    -h              Display usage information (this message)
+    """)
 
 def main():
     args = arguments_parse()
-    valid, cause = validate_arguments(args)
-    if not valid:
-        print(cause)
-        return 
     
 
 if __name__ == '__main__':
